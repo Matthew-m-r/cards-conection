f3787dd5b3a5c0dbcdeb87980e9d6e66dd546a7d
Matthew <teo-m.r@hotmail.com>
2023-01-28 00:46:23 -0500

    init project template

--- src/components/table/table.component.tsx
+++ src/components/table/table.component.tsx
@@ -1,9 +1,11 @@
 import "./table.styles.scss";
+import { cards } from "../../mockups/cards.data";
 
 const Table = () => {
     return (
         <div className="main-table-container">
             <h1>Hello from the table!</h1>
+            {/* {cards?.map()} */}
         </div>
     );
 };
